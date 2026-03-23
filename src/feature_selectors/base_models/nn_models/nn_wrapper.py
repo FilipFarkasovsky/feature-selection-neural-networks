28f908f7cb58346b0d2abdafedbce4365caaa91b
Filip Farkasovsky <farkasovsky.filip8@gmail.com>
2026-03-23 18:17:25 +0100

    Simplify nnwrapper.py by removing validation, scheduler, and early stopping

--- src/feature_selectors/base_models/nn_models/nn_wrapper.py
+++ src/feature_selectors/base_models/nn_models/nn_wrapper.py
@@ -1,25 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-#  nn_wrapper.py
-#  
-#  Copyright 2022 Antoine Passemiers <antoine.passemiers@gmail.com>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-
-import tqdm
 import captum.attr
 import numpy as np
 import torch
@@ -29,8 +7,6 @@ from torch.utils.data import DataLoader
 from .cancelout import CancelOut
 from .deeppink import DeepPINK
 from .utils import TrainingSet, TestSet
-from .sam import SharpnessAwareMinimizer
-
 
 def init_weights(m):
     if isinstance(m, torch.nn.Linear):
@@ -40,46 +16,13 @@ def init_weights(m):
             torch.nn.init.kaiming_uniform_(m.weight, nonlinearity='leaky_relu')
         m.bias.data.fill_(1e-3)
 
-
-class GaussianNoise(torch.nn.Module):
-
-    def __init__(self, stddev):
-        torch.nn.Module.__init__(self)
-        self.stddev = stddev
-
-    def forward(self, X):
-        if self.training:
-            X = X + self.stddev * torch.randn(*X.size())
-        return X
-
-
-"""
 class Model(torch.nn.Module):
 
-    def __init__(self, input_size, n_classes, latent_size=16):
-        torch.nn.Module.__init__(self)
-        n_out = 1 if (n_classes <= 2) else n_classes
-        self.layers = torch.nn.Sequential(
-            # torch.nn.LayerNorm(input_size),
-            GaussianNoise(0.1),
-            torch.nn.Linear(input_size, latent_size),
-            torch.nn.LeakyReLU(0.2),
-            torch.nn.Linear(latent_size, latent_size),
-            torch.nn.LeakyReLU(0.2),
-            torch.nn.Linear(latent_size, n_out))
-        self.apply(init_weights)
-            
-    def forward(self, x):
-        return self.layers(x)
-"""
-class Model(torch.nn.Module):
-
-    def __init__(self, input_size, n_classes, hidden_dims = None, gaussian_noise=0.7466805127272365, dropout=0.04308691548552568, layer_norm=0, activation='mish'):
+    def __init__(self, input_size, n_classes, hidden_dims = None, dropout=0.04308691548552568, activation='mish'):
         torch.nn.Module.__init__(self)
         n_out = 1 if (n_classes <= 2) else n_classes
         layers = []
-        if gaussian_noise > 0:
-            layers.append(GaussianNoise(gaussian_noise))
+
         inplace = False
 
         if hidden_dims is None:
@@ -88,13 +31,11 @@ class Model(torch.nn.Module):
         prev_dim = input_size
         for h_dim in hidden_dims:
 
-            if dropout > 0:
-                layers.append(torch.nn.Dropout(p=dropout, inplace=inplace))
 
             layers.append(torch.nn.Linear(prev_dim, h_dim))
 
-            if layer_norm:
-                layers.append(torch.nn.LayerNorm(h_dim))
+            if dropout > 0:
+                layers.append(torch.nn.Dropout(p=dropout, inplace=inplace))
 
             if activation == 'relu':
                 layers.append(torch.nn.ReLU(inplace=inplace))
@@ -152,130 +93,45 @@ class NNwrapper:
             X,
             Y,
             device='cpu',
-            learning_rate=0.0017601777068292975,  # 0.0005
-            epochs=416,  # 1000
-            batch_size=56,  # 64
-            weight_decay=0.00048519293899787247,  # 1e-5
-            val=0.2,
-            early_stopping_patience=66,  # 5
-            optimizer='adagrad',  # 'adam'
-            sam_type='no-sam'  # 'no-sam'
+            learning_rate=1e-3,
+            epochs=200,  
+            batch_size=64,  # 64
+            weight_decay=1e-5  # 1e-5
     ):
+        self.model.to(device)
 
-        if val > 0:
-            X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=val)
-        else:
-            X_train, y_train = X, Y
-            X_test = np.asarray([])
-            y_test = np.asarray([])
-
-        dataset = TrainingSet(X_train, y_train)
-        loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, sampler=None, num_workers=0)
-        
-        if val > 0:
-            val_dataset = TrainingSet(X_test, y_test)
-            val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, sampler=None, num_workers=0)
-        else:
-            val_loader = None
+        # Create dataloader for all samples
+        train_loader = DataLoader(
+            TrainingSet(X, Y),
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=0
+        )
+        criterion = torch.nn.BCEWithLogitsLoss() if self.n_classes <= 2 else torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
         self.model.train()
-        if self.n_classes <= 2:
-            criterion = torch.nn.BCEWithLogitsLoss(reduction='mean')
-        else:
-            criterion = torch.nn.NLLLoss(reduction='mean')
-
-        if optimizer == 'adam':
-            optimizer_class = torch.optim.Adam
-        elif optimizer == 'sgd':
-            optimizer_class = torch.optim.SGD
-        elif optimizer == 'rmsprop':
-            optimizer_class = torch.optim.RMSprop
-        elif optimizer == 'adamw':
-            optimizer_class = torch.optim.AdamW
-        else:
-            optimizer_class = torch.optim.Adagrad
-        if sam_type == 'no-sam':
-            optimizer = optimizer_class(self.model.parameters(), lr=learning_rate, weight_decay=weight_decay)
-        elif sam_type == 'sam':
-            optimizer = SharpnessAwareMinimizer(self.model.parameters(), optimizer_class, lr=learning_rate, weight_decay=weight_decay, adaptive=False)
-        else:
-            optimizer = SharpnessAwareMinimizer(self.model.parameters(), optimizer_class, lr=learning_rate, weight_decay=weight_decay, adaptive=True)
-
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            optimizer, mode='min', factor=0.9, patience=10, threshold=0.0001,
-            threshold_mode='rel', cooldown=5, min_lr=1e-5, eps=1e-08)
-
-        n_epochs_without_improvement = 0
-        state_dict_history = []
-        for e in range(epochs):
-
-            # Training error
-            total_error = 0
-            for x, y in loader:
+        for epoch in range(epochs):
+            total_loss = 0
+            for x, y in train_loader:
                 x = x.to(device)
                 y = y.to(device)
+
                 optimizer.zero_grad()
+                y_hat = self.model(x)
 
-                def closure():
-                    y_hat = self.model.forward(x)
-                    if self.n_classes > 2:
-                        y_hat = torch.log_softmax(y_hat, dim=1)
-                    else:
-                        y_hat = y_hat.reshape(len(y_hat))
+                if self.n_classes <= 2:
+                    y_hat = y_hat.squeeze()
+                    y = y.float()
 
-                    try:
-                        loss = criterion(y_hat, y)
-                    except RuntimeError:
-                        loss = criterion(y_hat, y.float())
+                loss = criterion(y_hat, y)
+                for cb in self.loss_callbacks:
+                    loss += cb()
 
-                    for loss_callback in self.loss_callbacks:
-                        loss = loss + loss_callback()  # Add regularisation terms
-                    loss.backward()
-                    return loss
+                loss.backward()
+                optimizer.step()
 
-                loss = closure()
-                if sam_type == 'no-sam':    
-                    optimizer.step()
-                else:
-                    optimizer.step(closure=closure)
-
-                total_error += loss.item()
-            scheduler.step(total_error)
-
-            if val_loader is not None:
-
-                # Validation error
-                with torch.no_grad():
-                    val_total_error = 0
-                    for x, y in val_loader:
-                        x = x.to(device)
-                        y = y.to(device)
-                        y_hat = self.model.forward(x)
-                        if self.n_classes > 2:
-                            y_hat = torch.log_softmax(y_hat, dim=1)
-                        else:
-                            y_hat = y_hat.reshape(len(y_hat))
-                        try:
-                            loss = criterion(y_hat, y)
-                        except RuntimeError:
-                            loss = criterion(y_hat, y.float())
-                        val_total_error += loss.item()
-
-                # Keep track of parameters
-                state_dict_history.append((val_total_error, self.model.state_dict()))
-                if len(state_dict_history) >= 2:
-                    if state_dict_history[-1][0] >= state_dict_history[-2][0]:
-                        n_epochs_without_improvement += 1
-                        if n_epochs_without_improvement >= early_stopping_patience:  # 5
-                            break
-                    else:
-                        n_epochs_without_improvement = 0
-
-
-        # Restore best parameters
-        if val > 0:
-            i = np.argmin([error for error, state_dict in state_dict_history])
-            self.model.load_state_dict(state_dict_history[i][1])
+                total_loss += loss.item()
 
         self.model.eval()
         self.trained = True
