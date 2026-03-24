51d87f223cd0910e1c4d0c1a5d4220d39f155ad3
Filip Farkasovsky <farkasovsky.filip8@gmail.com>
2026-03-24 02:01:21 +0100

    remove models that are not examined in my study

--- src/feature_selectors/base_models/__init__.py
+++ src/feature_selectors/base_models/__init__.py
@@ -1,11 +1,7 @@
 from .base_selector import BaseSelector, ResultType
 from .embedded import BaseEmbeddedFeatureSelector
 from .forward_feature_selector import ForwardFeatureSelector
-from .genetic_algorithm import GeneticAlgorithmFeatureSelector
 from .k_best import KBestFeatureSelector
-from .k_significant_best import KSignificantBestFeatureSelector
-from .multi_class_model import MultiClassModel
-from .pipeline import FeatureSelectorPipeline
 from .recursive_feature_elimination import RFE
 
 
@@ -13,10 +9,6 @@ __all__ = [
     BaseSelector, ResultType,
     BaseEmbeddedFeatureSelector,
     ForwardFeatureSelector,
-    GeneticAlgorithmFeatureSelector,
     KBestFeatureSelector,
-    KSignificantBestFeatureSelector,
-    MultiClassModel,
-    FeatureSelectorPipeline,
     RFE
 ]
