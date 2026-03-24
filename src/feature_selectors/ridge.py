717270107f0b4036287fe576b19b256cb0f4239d
Filip Farkasovsky <farkasovsky.filip8@gmail.com>
2026-03-24 01:50:33 +0100

    remove feature selectores which will not be included in the study

--- src/feature_selectors/ridge.py
+++
@@ -1,9 +0,0 @@
-from sklearn.linear_model import RidgeClassifier
-
-
-from feature_selectors.base_models.embedded import BaseEmbeddedFeatureSelector
-
-
-class RidgeClassifierFeatureSelector(BaseEmbeddedFeatureSelector):
-    def __init__(self, n_features=None, **kwargs):
-        super().__init__(RidgeClassifier(**kwargs), 'coef_', n_features=n_features)
